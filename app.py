36fb8a6dadf50a4badbdb48567b1ca58740e853f
sohuja96 <jsantillo3@gatech.edu>
2018-11-30 15:46:06 -0500

    update app

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 from flask import Flask
+import twitterAuth
+import requests
 app = Flask(__name__)
+
+trackUrl = "https://stream.twitter.com/1.1/statuses/filter.json"
+
+api = twitterAuth.api
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/health")
+def health():
+    print(api)
+    users = api.GetFriends()
+    string = ""
+    for user in users:
+        string += user.name + ", "
+    return api.VerifyCredentials().status.text
+
+@app.route("/track/<tag>",
+        methods=['GET'])
+def track(tag):
+    for i in api.GetStreamFilter(track=[tag]):
+        print(i.get("text"), "`|`")
